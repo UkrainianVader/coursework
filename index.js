2396c282593e69a7d64f3e40afb4ef668a14e48b
UkrainianVader <symphonicnightcore@gmail.com>
2026-02-26 21:29:39 +0200

    commit revert consequences

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //imports
-const db = require("./db.js")
+const db = require("./db/db_connections.js")
 const express = require('express')
-
+const app = express()
+const port = 3000
 
 let sql_query = "INSERT INTO test_table VALUES (123, 'Test text')";
 db.query(sql_query, function (err, result) {
@@ -9,13 +10,20 @@ db.query(sql_query, function (err, result) {
     console.log("1 record inserted");
   });
 
-const app = express()
-const port = 3000
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+    const sql = "SELECT * FROM test_table";
+    
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("DB error");
+        }
+        res.render('index', { items: results });
+    });
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
