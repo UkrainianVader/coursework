32881892190d470eae426106184289849268d0f8
UkrainianVader <symphonicnightcore@gmail.com>
2026-02-26 22:03:27 +0200

    express now shows table on webpage, for testing purposes tho

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 //imports
-const db = require("./db/db_connections.js")
+const db = require("./db/dbOperations.js")
 const express = require('express')
 const app = express()
 const port = 3000
 
-let sql_query = "INSERT INTO test_table VALUES (123, 'Test text')";
-db.query(sql_query, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });
+// let sql_query = "INSERT INTO test_table VALUES (123, 'Test text')";
+// db.query(sql_query, function (err, result) {
+//     if (err) throw err;
+//     console.log("1 record inserted");
+//   });
+
+db.insert("components", {id: 123, name: "I2C екран", type: "модуль", serial: "testserialnumber123", status: "використовується", description: "Ну типу екран"});
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    const sql = "SELECT * FROM test_table";
-    
-    db.query(sql, (err, results) => {
+    db.read("components", (err, results) => {
         if (err) {
             console.error(err);
             return res.status(500).send("DB error");
