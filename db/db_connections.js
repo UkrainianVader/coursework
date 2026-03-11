9d410fa142bdca6389f2b80d7492a341f2dad3b1
UkrainianVader <symphonicnightcore@gmail.com>
2026-03-11 09:18:39 +0200

    initializing empty db, some fixes in webpage

--- db/db_connections.js
+++ db/db_connections.js
@@ -11,7 +11,25 @@ let con = mysql.createConnection({
 });
 
 con.connect(function(err) {
-  if (err) throw err;
+  if (err){
+    if (err.code === 'ER_BAD_DB_ERROR') {
+    console.log("Database not found, creating...");
+    let tempCon = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: process.env.DB_PASSWORD
+    });
+    tempCon.connect(function(err) {
+      if (err) throw err;
+      tempCon.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`, function(err, result) {
+        if (err) throw err;
+        console.log("Database created or already exists");
+        tempCon.end();
+      });
+    });
+    }
+    else throw err;
+  }
   console.log("Connected!");
 });
 
