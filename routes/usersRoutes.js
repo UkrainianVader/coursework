29c48d31cd17ab294be6b38ef32454efe8e787c5
UkrainianVader <symphonicnightcore@gmail.com>
2026-04-02 12:12:47 +0300

    removing users

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const db = require('../db/dbOperations');
 const { requireAuth, requireAdmin } = require('../middleware/auth');
+const { route } = require('./componentsRoutes');
 
 const router = express.Router();
 
@@ -12,6 +13,22 @@ router.post('/add-user', requireAuth, requireAdmin, (req, res) => {
     const user = { username, password: hashedPassword, role };
 
     db.insert('users', '(username, password, role) VALUES (?, ?, ?)', user, (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send('Server error');
+        }
+        return res.redirect('/mainpage');
+    });
+});
+
+router.post('/delete-user', requireAuth, requireAdmin, (req, res) => {
+    const userId = Number(req.body.id ?? req.body.userId);
+
+    if (!Number.isInteger(userId) || userId <= 0) {
+        return res.status(400).send('Invalid user id');
+    }
+
+    db.remove('users', 'id', { id: userId }, (err) => {
         if (err) {
             console.log(err);
             return res.status(500).send('Server error');
